34c83120a7859730f21bb83b072f31d8845bbd94
VGShilling <wrong_vs@mail.ru>
2021-02-10 19:46:08 +1200

    UPD task_6

--- lesson-2/task_6.py
+++ lesson-2/task_6.py
@@ -1,9 +1,8 @@
 ls = list()
-# analysis_dict = dict()
-title_ls = list()
-price_ls = list()
-cnt_ls = list()
-measure_ls = list()
+title_ls = set()
+price_ls = set()
+cnt_ls = set()
+measure_ls = set()
 add = 'да'
 i = 1
 while add == 'да':
@@ -21,14 +20,14 @@ while add == 'да':
     add = input("Добавить еще один товар? (да/нет): ")
 for n in range(i-1):
     tup = ls[n]
-    title_ls.append(tup[1].get("название"))
-    price_ls.append(tup[1].get("цена"))
-    cnt_ls.append(tup[1].get("количество"))
-    measure_ls.append(tup[1].get("ед."))
-analysis_dict = {"название": title_ls,
-                 "цена": price_ls,
-                 "количество": cnt_ls,
-                 "ед.": measure_ls}
+    title_ls.add(tup[1].get("название"))
+    price_ls.add(tup[1].get("цена"))
+    cnt_ls.add(tup[1].get("количество"))
+    measure_ls.add(tup[1].get("ед."))
+analysis_dict = {"название": list(title_ls),
+                 "цена": list(price_ls),
+                 "количество": list(cnt_ls),
+                 "ед.": list(measure_ls)}
 print()
 print("Аналитика товаров:")
 for k, v in analysis_dict.items():
